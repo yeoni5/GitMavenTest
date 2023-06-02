23500edf5581f15ade631c7b883e5f988dd63de3
yeoni5 <yeoni5@naver.com>
2023-06-02 12:35:30 +0900

    sub() 추가

--- src/main/java/com/example/demo/CalcController.java
+++ src/main/java/com/example/demo/CalcController.java
@@ -15,4 +15,23 @@ public class CalcController {
 	public String add() {
 		return "25 + 36 = " + (25+36);
 	}
+	
+	@GetMapping(value="/sub")
+	public String sub() {
+		return "25 - 36 = " + (25-36);
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
